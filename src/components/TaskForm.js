c71ea3c8696d1e10a6b2b47e9bd67be327928acc
JanneHerrera <nitto1257@gmail.com>
2024-10-08 12:35:56 -0600

    done, the crud with redux works without a db

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTask } from "../features/tasks/TaskSlice";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTask, editTask } from "../features/tasks/TaskSlice";
 import { v4 as uuid } from "uuid";
+import { useNavigate, useParams } from "react-router-dom";
 
 function TaskForm() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const params = useParams();
+  const tasks = useSelector((state) => state.tasks);
 
   const [task, setTask] = useState({
     title: "",
@@ -20,21 +24,44 @@ function TaskForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addTask({ ...task, id: uuid() }));
+
+    if (params.id) {
+      dispatch(editTask(task));
+    } else {
+      dispatch(addTask({ ...task, id: uuid() }));
+    }
+    navigate("/");
   };
+
+  useEffect(() => {
+    if (params.id) {
+      setTask(tasks.find((task) => task.id === params.id));
+    }
+    console.log(params);
+  }, [params, tasks]);
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4 mb-2">
+      <label htmlFor="title" className="block text-sm font-bold">
+        Task:
+      </label>
       <input
         type="text"
         name="title"
         placeholder="title"
         onChange={handleChange}
+        value={task.title}
+        className="W-full p-2 rounded-md bg-zinc-600 mb-2"
       />
-      <input
+      <label htmlFor="description" className="block text-sm font-bold">
+        description:
+      </label>
+      <textarea
         type="text"
         name="description"
         placeholder="description"
         onChange={handleChange}
+        value={task.description}
+        className="W-full p-2 rounded-md bg-zinc-600 mb-2"
       />
       <br />
       <button type="submit"> submit</button>
